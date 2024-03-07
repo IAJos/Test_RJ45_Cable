9c1e4a6f7b98ed911e97c4f4b565e395a746517e
CHIMI Jospin <chimikotieu@gmail.com>
2024-03-07 15:40:44 -0400

    #4. Pin definition

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <Arduino.h>
+#include "Fil.hpp"
+#include "Cable.hpp"
+#include <vector>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LEFT_FIL_HALF_FULL_ORANGE 15
+#define LEFT_FIL_FULL_ORANGE 2
+#define LEFT_FIL_HALF_FULL_GREEN 4
+#define LEFT_FIL_FULL_BLUE 5
+#define LEFT_FIL_HALF_FULL_BLUE 18
+#define LEFT_FIL_FULL_GREEN 19
+#define LEFT_FIL_HALF_FULL_BRAIN 21
+#define LEFT_FIL_FULL_BRAIN 22
+
+#define RIGHT_FIL_HALF_FULL_ORANGE 13
+#define RIGHT_FIL_FULL_ORANGE 14
+#define RIGHT_FIL_HALF_FULL_GREEN 27
+#define RIGHT_FIL_FULL_BLUE 26
+#define RIGHT_FIL_HALF_FULL_BLUE 25
+#define RIGHT_FIL_FULL_GREEN 33
+#define RIGHT_FIL_HALF_FULL_BRAIN 32
+#define RIGHT_FIL_FULL_BRAIN 23
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,9 +28,4 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
